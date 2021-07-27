11320334334076452054e8ed86b40c0b12079d3f
jshorns <joey.s.hornsby@gmail.com>
2021-07-27 18:31:12 +0100

    started to do flask project

---
+++ helper.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+DB_PATH = './todo.db'
+NOTSTARTED = 'Not Started'
+INPROGRESS = 'In Progress'
+COMPLETED = 'Completed'
+
+
+def add_to_list(item):
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute('insert into items(item, status) values(?,?)', (item, NOTSTARTED))
+        conn.commit()
+        return {"item": item, "status": NOTSTARTED}
+    except Exception as e:
+        print('Error:', e)
+        return None
+
+
+def get_all_items():
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute('select * from items')
+        rows = c.fetchall()
+        return { "count": len(rows), "items": rows }
+    except Exception as e:
+        print('Error: ', e)
+        return None
+
+
+def get_item(item):
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("select status from items where item='%s'" % item)
+        status = c.fetchone()[0]
+        return status
+    except Exception as e:
+        print('Error: ', e)
+        return None
